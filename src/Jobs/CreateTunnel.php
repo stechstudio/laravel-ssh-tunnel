43b91885ba4f2102fefc512221c015f44388edea
Mike Pirog <mike@kalamuna.com>
2017-04-13 20:39:48 -0700

    Add the ability to inject SSH options

--- src/Jobs/CreateTunnel.php
+++ src/Jobs/CreateTunnel.php
@@ -29,8 +29,9 @@ class CreateTunnel
             config('tunneler.local_port')
         );
 
-        $this->sshCommand = sprintf('%s %s -N -i %s -L %d:%s:%d -p %d %s@%s',
+        $this->sshCommand = sprintf('%s %s %s -N -i %s -L %d:%s:%d -p %d %s@%s',
             config('tunneler.ssh_path'),
+            config('tunneler.ssh_options'),
             config('tunneler.ssh_verbosity'),
             config('tunneler.identity_file'),
             config('tunneler.local_port'),
